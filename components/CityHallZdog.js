e7f60131f2dfa1872e606ef327e9171156c83bb0
Ryan Rossiter <nayr0788@gmail.com>
2019-07-22 23:50:53 -0400

    Some progress

--- components/CityHallZdog.js
+++ components/CityHallZdog.js
@@ -1,82 +1,169 @@
-import React, { useRef } from 'react';
-import { Anchor, Ellipse, Cylinder, Box, Hemisphere, Rect, Group, Shape, Polygon, useRender } from 'react-zdog';
+import React, { Fragment, useRef } from 'react';
+import { Anchor, Ellipse, Cylinder, Box, Hemisphere, Rect, RoundedRect, Group, Shape, Polygon, useRender } from 'react-zdog';
 
 const LIMESTONE = "#E2E2DC";
 const LIMESTONE_DARK = "#cfcfc8";
-const ROOF = "#84B290";
+const GROUND = "#fff";
+const ROOF = "#707065";
+const ROOF_GREEN = "#96BAA0";
+const WINDOW = "#DBF0F9";
+const WINDOW_FRAME = "#C0CACE";
+
 const HEIGHT = 12;
+
+const Windows = ({w=6, h=2}) => {
+	let windows = [];
+	for (let x = 0; x < w; x++) {
+		for (let y = 0; y < h; y++) {
+			windows.push((
+				<Anchor translate={{x: (x - w / 2 + 0.5) * 2, y: (y - h / 2 + 0.5) * 3}}>
+					<RoundedRect width={1} height={2.2} fill color={WINDOW} />
+					<RoundedRect width={1} height={2.2} stroke={3} color={WINDOW_FRAME} />
+				</Anchor>
+			));
+		}
+	}
+
+	return (<Fragment>{windows.map(win=>win)}</Fragment>);
+};
+
+const Spire = ({color=ROOF, ...props}) => (
+	<Anchor {...props}>
+		<Cylinder diameter={1.5} length={1.5} color={LIMESTONE}
+			translate={{y: -1.5/2}} rotate={{x: Math.PI / 2}} />
+		<Hemisphere diameter={1.5} color={color} translate={{y: -1.5}} rotate={{x: Math.PI / 2}}/>
+	</Anchor>
+);
 
 const CityHallZdog = () => {
 	const ref = useRef(undefined);
 	useRender(() => {
+		ref.current.rotate.x = Math.sin(Date.now() / 1000) * Math.PI / 12;
 		ref.current.rotate.y += 0.01;
 	});
 
 	return (
 		<Anchor ref={ref} scale={10}>
-			{/*<Ellipse diameter={50} rotate={{x: Math.PI / 2}} translate={{y:5}} fill />*/}
+			<Group>
+				<Shape translate={{y: 50000000}} />
+				<Box width={14} height={3} depth={15} translate={{z: 15.5, y:1.5, x: 12}}
+					topFace="transparent" bottomFace={"#777"} color={LIMESTONE_DARK}/>
+				<Box width={14} height={3} depth={15} translate={{z: 15.5, y:1.5, x: -12}}
+					topFace="transparent" bottomFace={"#777"} color={LIMESTONE_DARK}/>
 
-			<Anchor translate={{y: -HEIGHT / 2}}>
-				<Box width={30} depth={10} height={HEIGHT} color={LIMESTONE} />
-				<Cylinder diameter={10} length={HEIGHT}	color={LIMESTONE}
-					translate={{x: 15}} rotate={{x: Math.PI / 2}}/>
-				<Box width={5} depth={5} height={HEIGHT} color={LIMESTONE} translate={{ x: 17.5, z: -2.5 }} />
-				<Cylinder diameter={10} length={HEIGHT}	color={LIMESTONE}
-					translate={{x: -15}} rotate={{x: Math.PI / 2}}/>
-				<Box width={5} depth={5} height={HEIGHT} color={LIMESTONE} translate={{ x: -17.5, z: -2.5 }} />
-
-				<Box width={10} depth={20} translate={{z:15}} height={HEIGHT} color={LIMESTONE} />
-			</Anchor>
-
-			<Group translate={{y: -HEIGHT / 2}}>
-				<Box width={10} depth={2} height={2} translate={{ z: -1 - 5, y: -HEIGHT/2 + 1}} color={LIMESTONE_DARK} />
-				<Cylinder diameter={1.5} length={HEIGHT} color={LIMESTONE_DARK}
-					translate={{z: -1 - 5, x: -4 }} rotate={{x: Math.PI / 2}}/>
-				<Cylinder diameter={1.5} length={HEIGHT} color={LIMESTONE_DARK}
-					translate={{z: -1 - 5, x: -1.5 }} rotate={{x: Math.PI / 2}}/>
-				<Cylinder diameter={1.5} length={HEIGHT} color={LIMESTONE_DARK}
-					translate={{z: -1 - 5, x: 1.5 }} rotate={{x: Math.PI / 2}}/>
-				<Cylinder diameter={1.5} length={HEIGHT} color={LIMESTONE_DARK}
-					translate={{z: -1 - 5, x: 4 }} rotate={{x: Math.PI / 2}}/>
+				<Rect width={300} height={300} translate={{z: -142}} rotate={{x: Math.PI / 2}} fill color={GROUND}/>
+				<Rect width={300} height={200} translate={{z: 123}} rotate={{x: Math.PI / 2}} fill color={GROUND}/>
+				<Rect width={100} height={15} translate={{z: 15.5, x: 69}} rotate={{x: Math.PI / 2}} fill color={GROUND}/>
+				<Rect width={100} height={15} translate={{z: 15.5, x: -69}} rotate={{x: Math.PI / 2}} fill color={GROUND}/>
 			</Group>
 
-			<Anchor translate={{y: -HEIGHT}}>
-				<Box width={10} depth={10} height={2} translate={{y: -1}} color={LIMESTONE_DARK}/>
-				<Box width={8} depth={8} height={2} translate={{y: -2 - 1}} color={ROOF}/>
+			<Group translate={{y: -HEIGHT}}>
+				<Anchor translate={{y: HEIGHT / 2}}>
+					<Box width={30} depth={10} height={HEIGHT} color={LIMESTONE} />
+					<Cylinder diameter={10} length={HEIGHT}	color={LIMESTONE}
+						translate={{x: 15}} rotate={{x: Math.PI / 2}}/>
+					<Box width={5} depth={5} height={HEIGHT} color={LIMESTONE} translate={{ x: 17.5, z: -2.5 }} />
+					<Cylinder diameter={10} length={HEIGHT}	color={LIMESTONE}
+						translate={{x: -15}} rotate={{x: Math.PI / 2}}/>
+					<Box width={5} depth={5} height={HEIGHT} color={LIMESTONE} translate={{ x: -17.5, z: -2.5 }} />
+				</Anchor>
+
+				<Anchor translate={{ y: - 1 }}>
+					<Box width={10} depth={10} height={2} translate={{}} color={LIMESTONE_DARK}/>
+
+					<Anchor translate={{ x: 11 }}>
+						<Rect width={12} height={5} fill color={ROOF}
+							translate={{z: 2.35}} rotate={{x: Math.PI / 2 - Math.PI / 8}} />
+						<Rect width={12} height={5} fill color={ROOF}
+							translate={{z: -2.35}} rotate={{x: Math.PI / 2 + Math.PI / 8}} />
+						<Spire translate={{y: -1}}/>
+					</Anchor>
+
+					<Anchor translate={{ x: -11 }}>
+						<Rect width={12} height={5} fill color={ROOF}
+							translate={{z: 2.35}} rotate={{x: Math.PI / 2 - Math.PI / 8}} />
+						<Rect width={12} height={5} fill color={ROOF}
+							translate={{z: -2.35}} rotate={{x: Math.PI / 2 + Math.PI / 8}} />
+						<Spire translate={{y: -1}}/>
+					</Anchor>
+				</Anchor>
+			</Group>
+
+			<Group translate={{ y: -HEIGHT - 1}}>
+				<Box width={8} depth={8} height={2} translate={{y: -1 - 1}} color={ROOF}/>
 				<Cylinder diameter={5} length={4} color={LIMESTONE}
-					translate={{y:-2 - 2 - 2}} rotate={{x: Math.PI / 2}} />
-				<Hemisphere diameter={5} color={ROOF} translate={{y:-2 - 2 - 4}} rotate={{x: Math.PI / 2}}/>
-				<Cylinder diameter={1} length={2} color={LIMESTONE}
-					translate={{y:-2 - 2 - 4 - 2.5 - 1}} rotate={{x: Math.PI / 2}} />
-				<Hemisphere diameter={1} color={ROOF} translate={{y:-2 - 2 - 4 - 2.5 - 2}} rotate={{x: Math.PI / 2}}/>
-				<Cylinder diameter={0.2} length={2} color={LIMESTONE_DARK}
-					translate={{y:-2 - 2 - 4 - 2.5 - 2 - 0.5 - 1}} rotate={{x: Math.PI / 2}} />
-
-				<Group translate={{ z: -5 }}>
-					<Rect width={5} height={2} fill color={ROOF}
-						translate={{z: -1, x: 2.35, y:-1}} rotate={{x: Math.PI / 2, y: -Math.PI / 8}} />
-					<Rect width={5} height={2} fill color={ROOF}
-						translate={{z: -1, x: -2.35, y:-1}} rotate={{x: Math.PI / 2, y: Math.PI / 8}} />
-					<Shape visible={false} translate={{z: -5, y: -5}}/>
-				</Group>
-
-				<Polygon radius={5} sides={3} fill translate={{z: -2 - 5, y: -0.7}} stroke={2} scale={{y: 0.23}} color={LIMESTONE_DARK} />
-			</Anchor>
-
-			<Group translate={{ x: 11, y: -HEIGHT - 1 }}>
-				<Rect width={12} height={5} fill color={ROOF}
-					translate={{z: 2.35}} rotate={{x: Math.PI / 2 - Math.PI / 8}} />
-				<Rect width={12} height={5} fill color={ROOF}
-					translate={{z: -2.35}} rotate={{x: Math.PI / 2 + Math.PI / 8}} />
-				<Shape visible={false} translate={{y: -50}}/>
+					translate={{y:-1 - 2 - 2}} rotate={{x: Math.PI / 2}} />
+				<Hemisphere diameter={5} color={ROOF} translate={{y:-1 - 2 - 4}} rotate={{x: Math.PI / 2}}/>
+				<Cylinder diameter={1.3} length={1.5} color={LIMESTONE}
+					translate={{y:-1 - 2 - 4 - 2.5 - 1.5/2}} rotate={{x: Math.PI / 2}} />
+				<Hemisphere diameter={1.3} color={ROOF} translate={{y:-1 - 2 - 4 - 2.5 - 1.5}} rotate={{x: Math.PI / 2}}/>
+				<Cylinder diameter={0.2} length={1} color={LIMESTONE_DARK}
+					translate={{y:-1 - 2 - 4 - 2.5 - 1.5 - 0.5 - 0.5}} rotate={{x: Math.PI / 2}} />
 			</Group>
 
-			<Group translate={{ x: -11, y: -HEIGHT - 1 }}>
-				<Rect width={12} height={5} fill color={ROOF}
-					translate={{z: 2.35}} rotate={{x: Math.PI / 2 - Math.PI / 8}} />
-				<Rect width={12} height={5} fill color={ROOF}
-					translate={{z: -2.35}} rotate={{x: Math.PI / 2 + Math.PI / 8}} />
-				<Shape visible={false} translate={{y: -50}}/>
+			<Group>
+				<Anchor translate={{y: -HEIGHT / 2}}>
+					<Box width={10} depth={2} height={2} translate={{ z: -1 - 5, y: -HEIGHT/2 + 1}} color={LIMESTONE_DARK} />
+					<Box width={10} depth={2} height={2} translate={{ z: -1 - 5, y: HEIGHT/2 - 1}} color={LIMESTONE_DARK} />
+					<Cylinder diameter={1.5} length={HEIGHT} color={LIMESTONE_DARK}
+						translate={{z: -1 - 5, x: -4 }} rotate={{x: Math.PI / 2}}/>
+					<Cylinder diameter={1.5} length={HEIGHT} color={LIMESTONE_DARK}
+						translate={{z: -1 - 5, x: -1.5 }} rotate={{x: Math.PI / 2}}/>
+					<Cylinder diameter={1.5} length={HEIGHT} color={LIMESTONE_DARK}
+						translate={{z: -1 - 5, x: 1.5 }} rotate={{x: Math.PI / 2}}/>
+					<Cylinder diameter={1.5} length={HEIGHT} color={LIMESTONE_DARK}
+						translate={{z: -1 - 5, x: 4 }} rotate={{x: Math.PI / 2}}/>
+				</Anchor>
+
+				<Anchor translate={{y: -HEIGHT -1}}>
+					<Polygon radius={5} sides={3} fill translate={{z: -2 - 5, y: 0.3}} stroke={2} scale={{y: 0.23}} color={LIMESTONE_DARK} />
+
+					<Anchor translate={{ z: -5 }}>
+						<Rect width={5} height={2} fill color={ROOF}
+							translate={{z: -1, x: 2.35 }} rotate={{x: Math.PI / 2, y: -Math.PI / 8}} />
+						<Rect width={5} height={2} fill color={ROOF}
+							translate={{z: -1, x: -2.35 }} rotate={{x: Math.PI / 2, y: Math.PI / 8}} />
+						<Shape visible={false} translate={{z: -5, y: -5}}/>
+					</Anchor>
+				</Anchor>
+
+				<Anchor translate={{y: -HEIGHT / 2, z: -5, x: 12}}>
+					<Windows />
+				</Anchor>
+				<Anchor translate={{y: -HEIGHT / 2, z: -5, x: -12}}>
+					<Windows />
+				</Anchor>
+			</Group>
+
+			<Group>
+				<Anchor translate={{y: -HEIGHT / 2, z: 5, x: -11}}>
+					<Windows />
+				</Anchor>
+				<Anchor translate={{y: -HEIGHT / 2, z: 5, x: 11}}>
+					<Windows />
+				</Anchor>
+			</Group>
+
+			<Group translate={{y: -HEIGHT / 2 + 1, z: 15, x: 5}} rotate={{y: Math.PI / 2}}>
+				<Windows w={9}/>
+			</Group>
+
+			<Group translate={{y: -HEIGHT / 2 + 1, z: 15, x: -5}} rotate={{y: Math.PI / 2}}>
+				<Windows w={9}/>
+			</Group>
+
+			<Group translate={{y: -HEIGHT+2}}>
+				<Anchor translate={{y: HEIGHT / 2 - 1}}>
+					<Box width={10} depth={20} translate={{z:15}} height={HEIGHT-2} color={LIMESTONE} />
+				</Anchor>
+
+				<Anchor translate={{ y: -1, z: 15 }}>
+					<Rect width={5} height={20} fill color={ROOF_GREEN}
+						translate={{x: 2.35}} rotate={{x: Math.PI / 2, y: -Math.PI / 8}} />
+					<Rect width={5} height={20} fill color={ROOF_GREEN}
+						translate={{x: -2.35}} rotate={{x: Math.PI / 2, y: Math.PI / 8}} />
+					<Spire translate={{y: -1}} color={ROOF_GREEN}/>
+				</Anchor>
 			</Group>
 		</Anchor>
 	);
